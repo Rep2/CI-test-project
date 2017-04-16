2545af135757733bedd70130ca950a6db044df28
Ivan Rep <ivan.rep2@gmail.com>
2017-04-16 19:45:24 +0200

    Initial Commit

---
+++ Diplomski_radTests/Diplomski_radTests.swift
@@ -0,0 +1,36 @@
+//
+//  Diplomski_radTests.swift
+//  Diplomski_radTests
+//
+//  Created by Ivan Rep on 16/04/2017.
+//  Copyright © 2017 rep. All rights reserved.
+//
+
+import XCTest
+@testable import Diplomski_rad
+
+class Diplomski_radTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
