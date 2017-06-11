011a39de1f9b961969301c5154224dbfc434158d
Ivan Rep <ivan.rep2@gmail.com>
2017-06-11 21:48:45 +0200

    Added couple of tests

--- Diplomski_radTests/Diplomski_radTests.swift
+++ Diplomski_radTests/Diplomski_radTests.swift
@@ -14,12 +14,4 @@ class Diplomski_radTests: XCTestCase {
     func testExample() {
         XCTAssert(true)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
